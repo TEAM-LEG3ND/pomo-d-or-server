e0ad6c1a68d396d1ceb0a055d9a44dbfa7f011ba
d0lim <limdongyoung0@gmail.com>
2023-08-15 17:15:15 +0900

    feat: init infrastructure module

---
+++ pomo-infrastructure/build.gradle.kts
@@ -0,0 +1,34 @@
+val exposedVersion: String by project
+val ktorVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("org.jmailen.kotlinter") version "3.16.0"
+}
+
+group = "com.leg3nd"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
