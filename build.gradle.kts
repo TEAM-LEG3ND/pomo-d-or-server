06317438142847e2fbf9297cd6bf8ca7d7b8fdaf
d0lim <limdongyoung0@gmail.com>
2023-08-15 11:07:10 +0900

    feat: create domain module

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,28 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.0"
     id("org.jmailen.kotlinter") version "3.16.0" apply false
-    application
 }
 
 group = "com.leg3nd"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
-}
-
-application {
-    mainClass.set("MainKt")
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
