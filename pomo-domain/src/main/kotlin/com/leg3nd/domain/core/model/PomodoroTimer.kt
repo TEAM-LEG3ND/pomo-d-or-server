edf679aa3c2b133ced45fd24a7843c9d5d142b20
d0lim <limdongyoung0@gmail.com>
2023-08-15 17:04:57 +0900

    feat: add status to PomodoroTimer

--- pomo-domain/src/main/kotlin/com/leg3nd/domain/core/model/PomodoroTimer.kt
+++ pomo-domain/src/main/kotlin/com/leg3nd/domain/core/model/PomodoroTimer.kt
@@ -6,6 +6,13 @@ data class PomodoroTimer(
     val id: Long,
     val author: User,
     val template: PomodoroTimerTemplate,
+    val status: Status,
     val createdAt: OffsetDateTime,
     val updatedAt: OffsetDateTime,
-)
+) {
+    enum class Status {
+        LIVE,
+        DONE,
+        TERMINATED,
+    }
+}
