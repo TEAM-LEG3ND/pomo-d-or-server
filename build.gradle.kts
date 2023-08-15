5ec3a401498b9c9bb5c2b9b648d0e7efa96ee148
d0lim <limdongyoung0@gmail.com>
2023-08-15 10:45:49 +0900

    feat: format setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.0"
+    id("com.diffplug.spotless") version "6.20.0"
     application
 }
 
@@ -26,4 +27,25 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+spotless {
+    // optional: limit format enforcement to just the files changed by this feature branch
+//    ratchetFrom("origin/main")
+
+    format("misc") {
+        // define the files to apply `misc` to
+        target("*.gradle", "*.md", ".gitignore")
+
+        // define the steps to apply to those files
+        trimTrailingWhitespace()
+        indentWithTabs() // or spaces. Takes an integer argument if you don't like 4
+        endWithNewline()
+    }
+
+    kotlin {
+        // version, userData and editorConfigOverride are all optional
+        ktlint("0.50.0")
+            .setEditorConfigPath("$projectDir/.editorconfig")
+    }
 }
