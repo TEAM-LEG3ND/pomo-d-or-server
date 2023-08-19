b7ce6fddd9306d1c8d59499ec79f6c0a5d2ea979
d0lim <limdongyoung0@gmail.com>
2023-08-19 14:41:49 +0900

    refactor: add default value to createdAt and updatedAt

--- pomo-infrastructure/src/main/kotlin/com/leg3nd/infrastructure/database/table/BaseTable.kt
+++ pomo-infrastructure/src/main/kotlin/com/leg3nd/infrastructure/database/table/BaseTable.kt
@@ -2,8 +2,13 @@ package com.leg3nd.infrastructure.database.table
 
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.javatime.timestamp
+import java.time.OffsetDateTime
 
 abstract class BaseTable : LongIdTable() {
-    val createdAt = timestamp("created_at")
-    val updatedAt = timestamp("updated_at")
+    val createdAt = timestamp("created_at").clientDefault {
+        OffsetDateTime.now().toInstant()
+    }
+    val updatedAt = timestamp("updated_at").clientDefault {
+        OffsetDateTime.now().toInstant()
+    }
 }
