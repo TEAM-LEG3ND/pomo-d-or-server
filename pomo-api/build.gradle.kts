48f9718b865e3c3a6a6a2e0a682878b29a323ef0
d0lim <limdongyoung0@gmail.com>
2023-08-22 21:41:10 +0900

    feat: configure build gradle

--- pomo-api/build.gradle.kts
+++ pomo-api/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    val kotlinVersion = "1.9.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
     id("io.ktor.plugin") version "2.3.3"
 }
 
@@ -15,6 +17,13 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+
+    implementation("io.github.smiley4:ktor-swagger-ui:2.3.1")
+
+    implementation("ch.qos.logback:logback-classic:1.4.11")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
