2c11fae700ce15ae09ecc03cf91dbed68c874da2
d0lim <limdongyoung0@gmail.com>
2023-08-27 01:40:13 +0900

    refactor: add userRoute to route config

--- pomo-api/src/main/kotlin/com/leg3nd/api/config/RoutesConfig.kt
+++ pomo-api/src/main/kotlin/com/leg3nd/api/config/RoutesConfig.kt
@@ -2,6 +2,7 @@ package com.leg3nd.api.config
 
 import com.leg3nd.api.routes.healthCheckRoute
 import com.leg3nd.api.routes.timerRoute
+import com.leg3nd.api.routes.userRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -9,5 +10,6 @@ fun Application.configureRoute() {
     routing {
         healthCheckRoute()
         timerRoute()
+        userRoute()
     }
 }
