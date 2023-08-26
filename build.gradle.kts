238f14664bc207d87b1b88fb70a281d4f598fd2c
d0lim <limdongyoung0@gmail.com>
2023-08-26 16:57:28 +0900

    feat: add koin with ksp for koin annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val koinVersion by properties
+val koinKspVersion by properties
+
 plugins {
     kotlin("jvm") version "1.9.0"
-    id("org.jmailen.kotlinter") version "3.16.0" apply false
+    id("org.jmailen.kotlinter") version "3.16.0"
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
 
 group = "com.leg3nd"
@@ -15,5 +19,42 @@ allprojects {
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("com.google.devtools.ksp")
+        plugin("org.jmailen.kotlinter")
+    }
+
+    dependencies {
+        implementation("io.insert-koin:koin-core:$koinVersion")
+        implementation("io.insert-koin:koin-annotations:$koinKspVersion")
+        ksp("io.insert-koin:koin-ksp-compiler:$koinKspVersion")
+    }
+
+    tasks.lintKotlinMain {
+        dependsOn("kspKotlin") // without this, it will fail to build with Gradle 8
+// I have to convert the type `FileCollection` to `FileTree` here.
+        source = (source - fileTree("$buildDir/generated")).asFileTree
+    }
+
+    tasks.formatKotlinMain {
+        dependsOn("kspKotlin") // without this, it will fail to build with Gradle 8
+// I have to convert the type `FileCollection` to `FileTree` here.
+        source = (source - fileTree("$buildDir/generated")).asFileTree
+    }
+
+    tasks.whenTaskAdded {
+//    println("adding task: $name")
+        if (name == "lintKotlinGeneratedByKspKotlin" ||
+            name == "lintKotlinGeneratedByKspTestKotlin" ||
+            name == "formatKotlinGeneratedByKspKotlin" ||
+            name == "formatKotlinGeneratedByKspTestKotlin"
+        ) {
+            enabled = false
+        }
     }
 }
--- pomo-api/build.gradle.kts
+++ pomo-api/build.gradle.kts
@@ -1,7 +1,10 @@
+val koinVersion by properties
+
 plugins {
     val kotlinVersion = "1.9.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
+    id("org.jmailen.kotlinter") version "3.16.0"
     id("io.ktor.plugin") version "2.3.3"
 }
 
@@ -12,8 +15,16 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass.set("com.leg3nd.api.Application")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
 dependencies {
     implementation(project(":pomo-domain"))
+    implementation(project(":pomo-infrastructure"))
 
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
@@ -23,10 +34,18 @@ dependencies {
 
     implementation("io.github.smiley4:ktor-swagger-ui:2.3.1")
 
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+
     implementation("ch.qos.logback:logback-classic:1.4.11")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+// Use KSP Generated sources
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
 }
 
 tasks.test {
