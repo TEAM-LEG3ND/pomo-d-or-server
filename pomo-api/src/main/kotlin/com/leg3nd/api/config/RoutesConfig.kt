39560153cc641b54996956516445410a7627766d
d0lim <limdongyoung0@gmail.com>
2023-08-26 17:00:27 +0900

    feat: add timerRoute to RoutesConfig

--- pomo-api/src/main/kotlin/com/leg3nd/api/config/RoutesConfig.kt
+++ pomo-api/src/main/kotlin/com/leg3nd/api/config/RoutesConfig.kt
@@ -1,11 +1,13 @@
 package com.leg3nd.api.config
 
 import com.leg3nd.api.routes.healthCheckRoute
+import com.leg3nd.api.routes.timerRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRoute() {
     routing {
         healthCheckRoute()
+        timerRoute()
     }
 }
